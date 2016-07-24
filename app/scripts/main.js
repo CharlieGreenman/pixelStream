bb44ded7262685e6f63d0da6e834982e04aa5397
Charlie Greenman <charie@razroo.com>
2016-07-24 00:16:29 -0400

    Fake dom to allow to snabbdom cycle app to output proper jsx html.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,8 +14,28 @@ function main() {
   return sinks;
 }
 
+function renderForm(sources) {
+    const form = {
+      DOM: xs.of(false).map(filler =>
+        <div styleName='choose-size' ref = "chooseSizeContainer">
+            <h6 styleName='header' >Choose size of grid</h6>
+            <form styleName='form'>
+              <label styleName='label'>columns</label>
+              <label styleName='label'>rows</label>
+              <input styleName='input' type="text" value = {20} defaultValue={20} id="input-for-columns"  />
+              <input styleName='input' onChange={20} type="text" value = {20} defaultValue={20} id="input-for-rows"  />
+              <label styleName='label'>Pixel Size</label>
+              <button className = 'button' id="create_grid" type="button" >Create Grid</button>
+              <input styleName='input' type="text" value = '20' defaultValue={20} id="input-for-pixel-size"  />
+            </form>
+        </div>
+      )
+    };
+    return form;
+}
+
 const drivers = {
   DOM: makeDOMDriver('#drawing_container')
 };
 
-run(main, drivers);
+run(renderForm, drivers);
