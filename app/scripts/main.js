d2bcfbd1407502eb6e88f01e03580796d74e1b66
Charlie Greenman <charie@razroo.com>
2016-08-02 23:41:41 -0400

    Add data to snabbdom jsx. Figure out why snabbdom jsx does not pick up on data tags.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -25,7 +25,7 @@ function getFormValue(domSource, className){
 function intent(domSource){
     return{
         changeColumnCount$: getFormValue(domSource, 'column'),
-        changeRowCount$: getFormValue(domSource, 'column'),
+        changeRowCount$: getFormValue(domSource, 'row'),
         changePixelCount$: getFormValue(domSource, 'pixel'),
     }
 }
@@ -38,11 +38,11 @@ function renderForm(sources) {
             <form className={styles.form}>
               <label className={styles.label}>columns</label>
               <label className={styles.label}>rows</label>
-              <input className={styles.input} type="text" value = {20} defaultValue={20} id="input-for-columns"  />
-              <input className={styles.input} onChange={20} type="text" value = {20} defaultValue={20} id="input-for-rows"  />
+              <input className={styles.input} type="text" value = {20} defaultValue={20} id="input-for-columns" data-type="column"  />
+              <input className={styles.input} onChange={20} type="text" value = {20} defaultValue={20} id="input-for-rows" data-type="row" />
               <label className={styles.label}>Pixel Size</label>
               <button className={styles.button} id="create_grid" type="button" >Create Grid</button>
-              <input className={styles.input} type="text" value = '20' defaultValue={20} id="input-for-pixel-size"  />
+              <input className={styles.input} type="text" value = '20' defaultValue={20} id="input-for-pixel-size" data-type="pixel" />
             </form>
         </div>
       )
